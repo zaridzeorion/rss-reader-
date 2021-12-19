a2d2140b656ed55ffc99223012c462b4c497aa92
Zurab Zaridze <zaridzezurab0@gmail.com>
2021-12-19 19:39:42 +0400

    Adding error handling

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-    $default_url = 'https://hnrss.org/frontpage';
-    $url = $default_url;
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $url = $_POST['url'];
-    };
+// Handling submit
+$url = 'https://hnrss.org/frontpage';
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    $url = $_POST['url'];
+};
 ?>
 
 <html>
     <head>
         <link rel="stylesheet" href="styles.css">
-        <title>rss feed reader</title>
+        <title>RSS feed reader</title>
     </head>
 
     <body>
@@ -22,22 +23,27 @@
         <hr>
         <main>
             <?php
-                $content = simplexml_load_file($url);
-
-                if($content) {
+                $content = @simplexml_load_file($url);
+                if (!$content)
+                {
+                    die('<h2 style="text-align: center;">No stats.</h2>');
+                }
+                else
+                {
                     echo '<ul>';
-                    echo '<h2>'. $content->channel->title . '</h2>';
+                    echo '<h2>' . $content
+                        ->channel->title . '</h2> <br>';
 
-                    foreach($content->channel->item as $item) {
+                    foreach ($content
+                        ->channel->item as $item)
+                    {
                         echo "<li>";
-                            echo "<h5>" . $item->title . "</h5>";
-                            echo "<p>" . $item->description ."</p>";
+                        echo "<h5>" . $item->title . "</h5>";
+                        echo "<p>" . $item->description . "</p>";
                         echo "</li>";
                     }
 
                     echo "</ul>";
-                }else {
-                    echo "<p>Please enter correct RSS feed URL.</p>";
                 }
             ?>
         </main>
